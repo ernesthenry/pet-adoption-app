18f99da4e981df2ad6cbc651880f3909508d78da
ernesthenry <henry38ernest@gmail.com>
2020-04-09 20:14:57 +0300

    Building django templates

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from .models import Pet
 
 
 def home(request):
-    return HttpResponse('<p>Home view</p>')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets})
 
 def pet_detail(request, id):
-    return HttpResponse('<p>Pet detail view with id {}</p>'.format(id))
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet Not found')
+    return render(request, 'pet_detail.html', {'pet': pet})
